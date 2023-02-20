34f266b183bb1eef7a8513b5dd759084ab36d95a
Archil Sikharulidzee <softdevelopment@yahoo.com>
2023-02-20 21:36:25 +0400

    updates

--- src/EscapeSequences.java
+++
@@ -1,9 +0,0 @@
-public class EscapeSequences {
-    public static void main(String[] args)
-    {
-        System.out.print("Welcome Mr. Archil to Java.\nYour are a guest here...");
-        System.out.println("Welcome \\ Archil");
-        System.out.println("Welcome to \"Home\" ");
-        System.out.println("\tWelcome with tabulation...");
-    }
-}
